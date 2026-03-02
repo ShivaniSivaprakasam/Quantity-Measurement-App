97ac1e5157b51d0ba587cf292751ce0c867ef937
shivani <shivaniavanikkarasi@gmail.com>
2026-03-02 10:57:09 +0530

    Initial project setup

--- src/main/java/com/bridgelabz/QuantityMeasurementApp.java
+++ src/main/java/com/bridgelabz/QuantityMeasurementApp.java
@@ -6,7 +6,22 @@ public class QuantityMeasurementApp {
 
         public Feet(double value) {
             this.value = value;
+        }
+        @Override
+        public boolean equals(Object obj) {
 
+            if (this == obj)
+                return true;
+
+            if (obj == null)
+                return false;
+
+            if (getClass() != obj.getClass())
+                return false;
+
+            Feet other = (Feet) obj;
+
+            return Double.compare(this.value, other.value) == 0;
         }
     }
 }
