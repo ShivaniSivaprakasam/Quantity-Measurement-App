92dcd3965e287c67579988fb887edd87d8e53df3
shivani <shivaniavanikkarasi@gmail.com>
2026-03-02 11:10:12 +0530

    [Shivani] : Added : UC1: Implement Feet measurement equality with full test coverage

--- src/test/java/com/bridgelabz/QuantityMeasurementAppTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurementAppTest.java
@@ -13,5 +13,26 @@ public class QuantityMeasurementAppTest {
         Feet f2 = new Feet(1.0);
         assertTrue(f1.equals(f2));
     }
-
+    @Test
+    public void testEquality_DifferentValue() {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(2.0);
+        assertFalse(f1.equals(f2));
     }
+    @Test
+    public void testEquality_NullComparison() {
+        Feet f1 = new Feet(1.0);
+        assertFalse(f1.equals(null));
+    }
+    @Test
+    public void testEquality_DifferentClass() {
+        Feet f1 = new Feet(1.0);
+        String value = "1.0";
+        assertFalse(f1.equals(value));
+    }
+    @Test
+    public void testEquality_SameReference() {
+        Feet f1 = new Feet(1.0);
+        assertTrue(f1.equals(f1));
+    }
+}
